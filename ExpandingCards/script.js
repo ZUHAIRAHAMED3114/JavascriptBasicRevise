bfc2e0aeffdf77d22f33bfc65cb2c6315aeae6ff
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-10-07 11:10:05 +0530

    Final Expadnig Script is Added

--- ExpandingCards/script.js
+++ ExpandingCards/script.js
@@ -1,2 +1,14 @@
-
-
+const panels = document.querySelectorAll('.panel')
+panels.forEach(x => {
+    x.addEventListener('click', e => {
+        MakeActive(x)
+    })
+});
+const MakeActive = (panel) => {
+    panels.forEach(x => {
+        if (x != panel && x.classList.contains('active')) {
+            x.classList.remove('active')
+        }
+    })
+    panel.classList.add('active');
+}
