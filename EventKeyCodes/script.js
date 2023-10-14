47d303b5a36a90c4b57d9b32393d24165a33801f
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-10-15 03:35:20 +0530

    Key Code is completed

--- EventKeyCodes/script.js
+++ EventKeyCodes/script.js
@@ -1,2 +1,24 @@
+const insert = document.getElementById('insert');
 
+window.addEventListener('keydown', (e) => {
+    console.log(e)
+    insert.innerHTML = getHtml(e);
+})
 
+getHtml = (e) => {
+    return `
+    <div class="key">
+     ${e.key===' '?'space':e.key}
+     <small>event.key</small>
+</div>
+<div class="key">
+${e.keyCode}<small>event.keyCode</small>
+</div>
+<div class="key">
+${e.code}<small>event.code</small>
+</div>
+<div class="key">
+    Press Any Key to Get the KeyCode
+</div>
+    `
+}
