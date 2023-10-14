7c939877f31853724cfcacad2d27ed2813e6c8c8
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-10-15 02:53:39 +0530

    Dad Joke Code Completed

--- DadJokes/script.js
+++ DadJokes/script.js
@@ -1,2 +1,13 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+generateJoke();
+jokeBtn.addEventListener('click', generateJoke);
 
+function generateJoke() {
+    fetch('https://icanhazdadjoke.com', { headers: { 'Accept': 'application/json' } })
+        .then(res => res.json())
+        .then(data => {
+            jokeEl.innerHTML = data.joke
 
+        });
+}
