b127cbdbfacc0d4bf51f48d3d76cf1db651b3eda
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-10-18 16:00:17 +0530

    Final commit

--- IncrementingCounter/script.js
+++ IncrementingCounter/script.js
@@ -1,2 +1,19 @@
+const counters = document.querySelectorAll('.counter');
+const updateCounter = (counter) => {
+    const target = +counter.getAttribute('data-target');
+    const c = +counter.innerText;
+
+    const increment = target / 2000;
+    if (c < target) {
+        counter.innerText = `${Math.ceil(c+increment)}`;
+        setTimeout(() => { updateCounter(counter) }, 1)
+    } else {
+        counter.innerText = target
+    }
 
 
+}
+counters.forEach(counter => {
+    counter.innerText = '0';
+    updateCounter(counter)
+});
