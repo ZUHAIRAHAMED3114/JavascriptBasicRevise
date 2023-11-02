b325f73a872bf41e247b2a666395d1a7a10b1ba9
ShaikZuhairAhamedd <zuhairshaik327@gmail.com>
2023-11-02 08:47:20 +0530

    Drag N Drop Completed

---
+++ DragNDrop/script.js
@@ -0,0 +1,53 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd);
+
+
+for (var empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+    var dragElement = this;
+    dragElement.classList.add('hold');
+    setTimeout(() => {
+        //dragElement.style.display = "none";
+        this.className = "invisible"
+
+    }, 0)
+
+
+}
+
+function dragEnd() {
+
+    var dragElement = this;
+    dragElement.className = 'fill';
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+}
+
+function dragLeave() {
+    this.className = 'empty'
+}
+
+function dragDrop() {
+    console.log('drag Drop');
+    var dragElement = this;
+    console.log(dragElement);
+
+    dragElement.className = 'empty'
+    this.append(fill)
+}
